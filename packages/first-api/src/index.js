39b25676530b9e9fe9313a6223b8f414e0930be6
Mauricio <mauricio.r.andre@gmail.com>
2021-07-25 13:18:21 -0300

    Manipulando dados dos projetos

--- packages/first-api/src/index.js
+++ packages/first-api/src/index.js
@@ -1,21 +1,61 @@
 const express = require('express');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
+app.use(express.json());
+
+const projects = [];
+
 app.get('/projects', (request, response) => {
-  return response.json(['Projeto 1', 'Projeto 2']);
+  const { title } = request.query;
+
+  const result = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+
+  return response.json(result);
 });
 
 app.post('/projects', (request, response) => {
-  return response.json(['Projeto 1', 'Projeto 2', 'Projeto 3']);
+  const { title, owner } = request.body;
+
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+
+  return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
-  return response.json(['Projeto 4', 'Projeto 2', 'Projeto 3']);
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' });
+  }
+
+  const project = { id, title, owner };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json(['Projeto 2', 'Projeto 3']);
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
