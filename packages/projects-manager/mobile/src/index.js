a567c6702b0e82c7e3295d58bb4b71a12440e5a1
Mauricio <mauricio.r.andre@gmail.com>
2021-09-04 16:26:09 -0300

    Porjeto mobile module 03

--- packages/projects-manager/mobile/src/index.js
+++ packages/projects-manager/mobile/src/index.js
@@ -1,6 +1,75 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  StatusBar,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import api from './services/api';
 
 export default function App() {
-  return <View />
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('/projects', {
+      title: `Projeto mobile ${Date.now()}`,
+      owner: 'Mauricio Andre',
+    });
+
+    setProjects([...projects, response.data]);
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.6}
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+  },
+  project: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
