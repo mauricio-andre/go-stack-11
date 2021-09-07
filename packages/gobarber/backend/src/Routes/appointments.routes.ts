a0720d33de77e731f9a7f2e1ea56b4dc799e7c7c
Mauricio <mauricio.r.andre@gmail.com>
2021-09-07 14:07:01 -0300

    Trabalhando com datas

--- packages/gobarber/backend/src/Routes/appointments.routes.ts
+++ packages/gobarber/backend/src/Routes/appointments.routes.ts
@@ -1,22 +1,39 @@
 import { Router } from 'express';
 import { v4 } from 'uuid';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(appointment.date, parsedDate),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
 
   const appointment = {
     id: v4(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
 
-  response.json(appointment);
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
