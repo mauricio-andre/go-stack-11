e313ce71097058579314ccf80a9b6aef57ab2df0
Mauricio <mauricio.r.andre@gmail.com>
2021-09-07 15:01:54 -0300

    Criando serviço para inclusão ed appointment

---
+++ packages/gobarber/backend/src/services/CreateAppointmentService.ts
@@ -0,0 +1,35 @@
+import { startOfHour } from 'date-fns';
+import Appointment from '../entities/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface RequestDto {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ provider, date }: RequestDto): Appointment {
+    const appointmentDate = startOfHour(date);
+    const findAppointmentInSameDate =
+      this.appointmentsRepository.findByDate(appointmentDate);
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
