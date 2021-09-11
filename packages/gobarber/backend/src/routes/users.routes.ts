58c08a973afa42d1b9483c87cca34f0e23bf5131
Mauricio <mauricio.r.andre@gmail.com>
2021-09-11 14:50:34 -0300

    Criptografando a senha

--- packages/gobarber/backend/src/routes/users.routes.ts
+++ packages/gobarber/backend/src/routes/users.routes.ts
@@ -1,6 +1,11 @@
 import { Router } from 'express';
+import User from '../entities/User';
 
 import CreateUserService from '../services/CreateUserService';
+
+interface UserResponse extends Omit<User, 'password'> {
+  password?: string;
+}
 
 const usersRouter = Router();
 
@@ -11,7 +16,12 @@ usersRouter.post('/', async (request, response) => {
     const createUser = new CreateUserService();
 
     const user = await createUser.execute({ name, email, password });
-    return response.json(user);
+
+    const userResponse = { ...user } as UserResponse;
+
+    delete userResponse.password;
+
+    return response.json(userResponse);
   } catch ({ message }) {
     return response.status(400).json({ error: message });
   }
