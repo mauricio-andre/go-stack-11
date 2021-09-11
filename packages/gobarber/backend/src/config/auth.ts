aa3bef4a7fd276cae01b30f7db14cde7239e65a4
Mauricio <mauricio.r.andre@gmail.com>
2021-09-11 17:24:41 -0300

    Validando token gerado

---
+++ packages/gobarber/backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '8e29b4b7f542d7c3682086978c878f3f',
+    expiresIn: '1d',
+  },
+};
--- packages/gobarber/backend/src/services/AuthenticateUserService.ts
+++ packages/gobarber/backend/src/services/AuthenticateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import User from '../entities/User';
+import authConfig from '../config/auth';
 
 interface Request {
   email: string;
@@ -31,9 +32,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    const token = sign({}, '8e29b4b7f542d7c3682086978c878f3f', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
