6ceff03ce9ac919e9bbda44cfc90a4bdc73b7718
Mauricio <mauricio.r.andre@gmail.com>
2021-10-10 15:16:27 -0300

    Correção conversão de parâmetro

--- packages/gobarber/backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ packages/gobarber/backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -16,19 +16,15 @@ export default class ProviderAppointmentsController {
   public async index(request: IRequest, response: Response): Promise<Response> {
     const userId = request.user.id;
     const { day, month, year } = request.query;
-    const parsedDay = parseInt(day, 10);
-    const parsedMonth = parseInt(month, 10);
-    const parsedYear = parseInt(year, 10);
-
     const listProviderAppointment = container.resolve(
       ListProviderAppointmentService,
     );
 
     const appointment = await listProviderAppointment.execute({
       providerId: userId,
-      day: parsedDay,
-      month: parsedMonth,
-      year: parsedYear,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(appointment);
