7f2c0ea748558bcce6824a61ec725b4b75064e78
Mauricio <mauricio.r.andre@gmail.com>
2021-10-12 16:20:26 -0300

    Criado arquivo das novas páginas

--- packages/gobarber/mobile/src/routes/app.routes.tsx
+++ packages/gobarber/mobile/src/routes/app.routes.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import React from 'react';
+import AppointmentCreated from '../pages/AppointmentCreated';
+import CreateAppointment from '../pages/CreateAppointment';
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 const App = createNativeStackNavigator<RootStackParamList>();
 
@@ -14,6 +16,10 @@ const AppRoutes: React.FC = () => {
       }}
     >
       <App.Screen name="Dashboard" component={Dashboard} />
+      <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+      <App.Screen name="CreateAppointment" component={CreateAppointment} />
+
+      <App.Screen name="Profile" component={Profile} />
     </App.Navigator>
   );
 };
