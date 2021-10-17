4dbcc38a6ca6bc076a53bdc3553b676c7cf3f150
Mauricio <mauricio.r.andre@gmail.com>
2021-10-17 11:57:01 -0300

    Header tela de provider

--- packages/gobarber/mobile/src/pages/CreateAppointment/index.tsx
+++ packages/gobarber/mobile/src/pages/CreateAppointment/index.tsx
@@ -1,6 +1,44 @@
-import React from 'react';
-import { View } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { MyNavigationProps } from '../../@types/MyNavigationProps';
+import { useAuth } from '../../context/AuthContext';
 
-const CreateAppointment: React.FC = () => <View />;
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
+
+interface routeParams {
+  providerId: string;
+}
+
+const CreateAppointment: React.FC = () => {
+  const { user } = useAuth();
+  const route = useRoute();
+  const { goBack } = useNavigation<MyNavigationProps>();
+  const { providerId } = route.params as routeParams;
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiro</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatarUrl }} />
+      </Header>
+    </Container>
+  );
+};
 
 export default CreateAppointment;
