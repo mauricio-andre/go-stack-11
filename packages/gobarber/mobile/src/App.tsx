0f514464bd52f9f5fe2b704d95777a365d077d5e
Mauricio <mauricio.r.andre@gmail.com>
2021-11-15 12:09:11 -0300

    Incluido icone e splashScreen no android

--- packages/gobarber/mobile/src/App.tsx
+++ packages/gobarber/mobile/src/App.tsx
@@ -1,19 +1,30 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen';
 import Routes from './routes';
 import AppProvider from './context';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#312e38" translucent />
-    <GestureHandlerRootView style={{ backgroundColor: '#312e38', flex: 1 }}>
-      <AppProvider>
-        <Routes />
-      </AppProvider>
-    </GestureHandlerRootView>
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#312e38"
+        translucent
+      />
+      <GestureHandlerRootView style={{ backgroundColor: '#312e38', flex: 1 }}>
+        <AppProvider>
+          <Routes />
+        </AppProvider>
+      </GestureHandlerRootView>
+    </NavigationContainer>
+  );
+};
 
 export default App;
